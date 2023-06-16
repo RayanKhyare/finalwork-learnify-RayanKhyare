b5adcdf37fd89a4aa7ecc397e55161744f075bd7
Rayan Khyare <rayan.khyare@student.ehb.be>
2023-06-16 18:41:29 +0200

    Changes to deploy

--- learnify-backend/index.js
+++ learnify-backend/index.js
@@ -1,10 +1,10 @@
 const app = require("./app.js");
 const socketServer = require("./socketServer");
 
-const SERVERPORT = 4000;
+const PORT = 4000;
 const SOCKETPORT = 5000;
 
-app.listen(SERVERPORT, () => console.log("Server up and running"));
+app.listen(PORT, () => console.log("Server up and running"));
 
 // Start the SocketIo server
 socketServer.listen(SOCKETPORT, () => {
