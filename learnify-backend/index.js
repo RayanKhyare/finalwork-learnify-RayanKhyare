963e3614eb35d7ab90d48f85734b3a917f42b9a3
Rayan Khyare <rayan.khyare@student.ehb.be>
2023-06-16 18:35:10 +0200

    Updated port names

--- learnify-backend/index.js
+++ learnify-backend/index.js
@@ -1,10 +1,10 @@
 const app = require("./app.js");
 const socketServer = require("./socketServer");
 
-const PORT = process.env.PORT || 4000;
-const SOCKETPORT = process.env.PORT || 5000;
+const SERVERPORT = 4000;
+const SOCKETPORT = 5000;
 
-app.listen(PORT, () => console.log("Server up and running"));
+app.listen(SERVERPORT, () => console.log("Server up and running"));
 
 // Start the SocketIo server
 socketServer.listen(SOCKETPORT, () => {
